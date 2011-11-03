94ff1f82f9244995edc1050ce8236fa89287ea5e
Allyson Barros <allysonbarrosrn@gmail.com>
2011-11-03 16:18:34 -0300

    -a

--- ejbProject/ejbModule/br/edu/ifrn/qestagio/modelo/Usuario.java
+++ ejbProject/ejbModule/br/edu/ifrn/qestagio/modelo/Usuario.java
@@ -1,6 +1,13 @@
 package br.edu.ifrn.qestagio.modelo;
 
-import javax.persistence.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 public class Usuario {
 	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE)
@@ -11,4 +18,11 @@ public class Usuario {
 	
 	public String login;
 	public String senha;
+	public boolean ativado;
+	
+	private void criptografarSenha() throws NoSuchAlgorithmException {
+		MessageDigest digest = MessageDigest.getInstance("MD5");
+	    digest.update(senha.getBytes(), 0, senha.length());
+	    this.senha = new BigInteger(1, digest.digest()).toString(16);
+	}
 }
