daff6a3ded5dec0a98a4dadf5cddf5c53ba8aa41
Allyson Barros <allysonbarrosrn@gmail.com>
2011-11-19 14:11:41 -0300

    removendo sujeira do repositório.

--- ejbProject/ejbModule/br/edu/ifrn/qestagio/modelo/TipoPessoa.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifrn.qestagio.modelo;
-
-public class TipoPessoa {
-	public static final int ALUNO = 0;
-	public static final int FUNCIONARIO = 1;
-	public static final int ORIENTADOR = 2;
-	
-}
