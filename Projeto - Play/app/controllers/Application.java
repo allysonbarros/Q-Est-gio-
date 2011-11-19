ba51ecec468c18b526f318172e846c48766c96d1
Allyson Barros <allysonbarrosrn@gmail.com>
2011-11-19 16:00:03 -0300

    Implementando formulário de cadastro de aluno.

--- Projeto - Play/app/controllers/Application.java
+++ Projeto - Play/app/controllers/Application.java
@@ -1,41 +1,13 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import helpers.Contexto;
-
-import java.util.*;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import br.edu.ifrn.negocio.Endereco;
-import br.edu.ifrn.patterns.EnderecoDelegate;
+import play.mvc.Controller;
 
 public class Application extends Controller {
 
     public static void index() throws NamingException {
-    	EnderecoDelegate enderecoBean;
-    	
-		try {
-			enderecoBean = new EnderecoDelegate();
-			System.out.println(enderecoBean.toString());
-			
-			Endereco e = new Endereco();
-			e.setLogradouro("Rua Santa Luzia");
-			e.setNumero("101A");
-			e.setBairro("Quintas");
-			e.setCidade("Natal");
-			e.setUf("RN");
-			e.setCep("59035-186");
-			
-			enderecoBean.cadastrarEndereco(e);
-			
-		} catch (Exception e) {
-			error(e);
-		}
+    	render();
     }
     
 }
---
+++ Projeto - Play/app/controllers/Usuarios.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.*;
+
+public class Usuarios extends Controller {
+
+    public static void index() {
+        render();
+    }
+    
+    public static void formCadastro() {
+    	render();
+    }
+
+}
