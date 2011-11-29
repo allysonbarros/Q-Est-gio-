dea82a373bfe1987830b305894dac90f820652ab
Sedir Morais <philippi.sedir@gmail.com>
2011-11-29 20:59:44 -0300

    Classes Estagio e OfertaEstagio, e hibernate.cfg atualizados

--- Projeto - EJB/src/br/edu/ifrn/negocio/Estagio.java
+++ Projeto - EJB/src/br/edu/ifrn/negocio/Estagio.java
@@ -1,16 +1,70 @@
 package br.edu.ifrn.negocio;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.SequenceGenerator;
 
 @Entity
 @SequenceGenerator(name="ESTAGIO_SeqGen", sequenceName="ESTAGIO_GEN_VAL")
 public class Estagio {
-	Empresa empresa;
-	Pessoa orientador;
-	Pessoa aluno;
+	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="ESTAGIO_SeqGen")
+	private Long id;
 	
-	String areaConhecimento;
-	String funcao;
-	String descricao;
+	@JoinColumn(name="empresa_id")
+	private Empresa empresa;
+	@JoinColumn(name="orientador_id")
+	private Pessoa orientador;
+	@JoinColumn(name="aluno_id")
+	private Pessoa aluno;
+	
+	private String areaConhecimento;
+	private String funcao;
+	private String descricao;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	public Pessoa getOrientador() {
+		return orientador;
+	}
+	public void setOrientador(Pessoa orientador) {
+		this.orientador = orientador;
+	}
+	public Pessoa getAluno() {
+		return aluno;
+	}
+	public void setAluno(Pessoa aluno) {
+		this.aluno = aluno;
+	}
+	public String getAreaConhecimento() {
+		return areaConhecimento;
+	}
+	public void setAreaConhecimento(String areaConhecimento) {
+		this.areaConhecimento = areaConhecimento;
+	}
+	public String getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
 }
--- Projeto - EJB/src/br/edu/ifrn/negocio/OfertaEstagio.java
+++ Projeto - EJB/src/br/edu/ifrn/negocio/OfertaEstagio.java
@@ -4,16 +4,62 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.SequenceGenerator;
 
 @Entity
 @SequenceGenerator(name="OFERTAESTAGIO_SeqGen", sequenceName="OFERTAESTAGIO_GEN_VAL")
 public class OfertaEstagio  implements Serializable {
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -1651515689960732974L;
+
+	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="OFERTAESTAGIO_SeqGens")
+	private Long id;
 	
-	Empresa empresa;
-	List<Pessoa> candidatos;
-	String areaConhecimento;
-	String funcao;
-	String descricao;
+	@JoinColumn(name="empresa_id")
+	private Empresa empresa;
+	private List<Pessoa> candidatos;
+	private String areaConhecimento;
+	private String funcao;
+	private String descricao;
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	public List<Pessoa> getCandidatos() {
+		return candidatos;
+	}
+	public void setCandidatos(List<Pessoa> candidatos) {
+		this.candidatos = candidatos;
+	}
+	public String getAreaConhecimento() {
+		return areaConhecimento;
+	}
+	public void setAreaConhecimento(String areaConhecimento) {
+		this.areaConhecimento = areaConhecimento;
+	}
+	public String getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
 }
