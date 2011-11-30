970d6d0f6b15ade2db126201d5fae879b4e30b5a
Allyson Barros <allysonbarrosrn@gmail.com>
2011-11-30 13:40:23 -0300

    Adicionando conteúdo na página principal

--- Projeto - Play/app/controllers/Application.java
+++ Projeto - Play/app/controllers/Application.java
@@ -14,6 +14,7 @@ import br.edu.ifrn.patterns.ServiceLocator;
 import play.mvc.Controller;
 import play.mvc.With;
 
+@With(SessionsHelper.class)
 public class Application extends Controller {
 
     public static void index() {
