ef85fd24d9a6b0b17bc099b4101693be3bf0f62a
Sedir Morais <philippi.sedir@gmail.com>
2012-02-12 16:31:14 -0300

    Criar relacionamentos entre Aluno e Curso, e Diretoria e Orientador

--- Projeto - EJB/src/br/edu/ifrn/negocio/Aluno.java
+++ Projeto - EJB/src/br/edu/ifrn/negocio/Aluno.java
@@ -5,6 +5,7 @@ package br.edu.ifrn.negocio;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 
 @Entity
@@ -12,13 +13,27 @@ import javax.persistence.Entity;
 public class Aluno extends Pessoa {
 	
 	private static final long serialVersionUID = -9151016583503663685L;
-	private String teste;
-	
+	@ManyToOne
+	private Curso curso;
 	
 	//private OfertaEstagio oferta;
 	
 	public Aluno(){
 		
+	}
+
+	/**
+	 * @return the curso
+	 */
+	public Curso getCurso() {
+		return curso;
+	}
+
+	/**
+	 * @param curso the curso to set
+	 */
+	public void setCurso(Curso curso) {
+		this.curso = curso;
 	}
 
 	
--- Projeto - EJB/src/br/edu/ifrn/negocio/Orientador.java
+++ Projeto - EJB/src/br/edu/ifrn/negocio/Orientador.java
@@ -2,10 +2,12 @@ package br.edu.ifrn.negocio;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 @DiscriminatorValue((TipoPessoa.ORIENTADOR+""))	
 public class Orientador extends Pessoa {
 	private static final long serialVersionUID = 2108646997187776609L;
-	private String teste;
+	@ManyToOne
+	private Diretoria diretoria;
 }
