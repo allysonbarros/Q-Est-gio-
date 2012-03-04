29bc71897b79282ded55cb13ccd8f5b6f8de9e2a
Daniele Montenegro <monte.daniele@gmail.com>
2012-03-03 21:43:24 -0300

    Adcionando a Classe Habilidade, e fazendo o relacionamento com a Aluno

---
+++ Projeto - EJB/src/br/edu/ifrn/negocio/Habilidade.java
@@ -0,0 +1,51 @@
+package br.edu.ifrn.negocio;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+@SequenceGenerator(name="HABILIDADE_SeqGen", sequenceName="HABILIDADE_GEN_VAL")
+public class Habilidade implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="HABILIDADE_SeqGen")
+	private Long id;
+
+	private String nome;
+	private String nivel;
+	
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+
+	public String getNivel() {
+		return nivel;
+	}
+
+}
