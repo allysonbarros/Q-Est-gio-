2aa54d7395759f03f0f9719f58039d5d76792476
Sedir Morais <philippi.sedir@gmail.com>
2012-03-05 19:05:53 -0300

    Adicionado ver detalhes de oferta estágio

--- Projeto - Play/app/controllers/Alunos.java
+++ Projeto - Play/app/controllers/Alunos.java
@@ -17,6 +17,7 @@ import br.edu.ifrn.negocio.TipoPessoa;
 import br.edu.ifrn.patterns.AlunoDelegate;
 import br.edu.ifrn.patterns.CursoDelegate;
 import br.edu.ifrn.patterns.DiretoriaDelegate;
+import br.edu.ifrn.negocio.Habilidade.TipoHabilidade;
 @With(SessionsHelper.class)
 public class Alunos extends Controller {
 
